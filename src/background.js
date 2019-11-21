01d450e89e86f90e32022413e184815de5306444
Harry Chang <harry830622@gmail.com>
2019-11-22 00:56:28 +0800

    Change team&stats in matchup page

--- src/background.js
+++ src/background.js
@@ -17,7 +17,7 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-const baseUrl = 'https://basketball.fantasysports.yahoo.com/nba';
+const baseUrl = 'https://basketball.fantasysports.yahoo.com';
 
 const toDoc = html => {
   const domParser = new DOMParser();
@@ -36,19 +36,18 @@ chrome.runtime.onConnect.addListener(port => {
     switch (type) {
       case 'INIT': {
         const { leagueId } = payload;
-        getDoc(`${baseUrl}/${leagueId}`)
+        getDoc(`${baseUrl}/nba/${leagueId}`)
           .then(doc => {
             const matchupHrefs = [
               ...doc.querySelectorAll('#matchupweek ul > li'),
             ].map(n => n.getAttribute('data-target'));
-            console.log(matchupHrefs);
             const teamIds = [
               ...doc.querySelectorAll('#matchupweek ul > li a.F-link'),
             ].map(n => n.getAttribute('href').split('/')[3]);
             return Promise.all([
               Promise.all(
                 teamIds.map(teamId =>
-                  getDoc(`${baseUrl}/${leagueId}/${teamId}`).then(doc => {
+                  getDoc(`${baseUrl}/nba/${leagueId}/${teamId}`).then(doc => {
                     const teamDiv = doc.querySelector('#team-card-info');
                     const teamName = teamDiv.querySelector(
                       `a[href="/nba/${leagueId}/${teamId}/team"]`,
@@ -66,24 +65,133 @@ chrome.runtime.onConnect.addListener(port => {
                       .querySelector('img[class*="Avatar"]')
                       .getAttribute('src');
                     return {
-                      id: teamId,
-                      name: teamName,
-                      user: teamUser,
-                      standing: teamStanding,
-                      record: teamRecord,
-                      avatarUrl: teamAvatarUrl,
+                      [teamId]: {
+                        name: teamName,
+                        user: teamUser,
+                        standing: teamStanding,
+                        record: teamRecord,
+                        avatarUrl: teamAvatarUrl,
+                      },
                     };
                   }),
                 ),
               ),
+              Promise.all(
+                matchupHrefs.map(matchupHref =>
+                  getDoc(`${baseUrl}${matchupHref}`).then(doc => {
+                    const matchupWallHeader = doc.querySelector(
+                      '#matchup-wall-header',
+                    );
+                    const team1Row = matchupWallHeader.querySelector(
+                      'tbody tr:nth-child(1)',
+                    );
+                    const team1Id = team1Row
+                      .querySelector('td:nth-child(1) span > a')
+                      .getAttribute('href')
+                      .split('/')[3];
+                    const team1Fgp = window.parseFloat(
+                      team1Row.querySelector('td:nth-child(3) div').innerText,
+                    );
+                    const team1Ftp = window.parseFloat(
+                      team1Row.querySelector('td:nth-child(5) div').innerText,
+                    );
+                    const team1Tptm = window.parseInt(
+                      team1Row.querySelector('td:nth-child(6) div').innerText,
+                    );
+                    const team1Pts = window.parseInt(
+                      team1Row.querySelector('td:nth-child(7) div').innerText,
+                    );
+                    const team1Reb = window.parseInt(
+                      team1Row.querySelector('td:nth-child(8) div').innerText,
+                    );
+                    const team1Ast = window.parseInt(
+                      team1Row.querySelector('td:nth-child(9) div').innerText,
+                    );
+                    const team1Stl = window.parseInt(
+                      team1Row.querySelector('td:nth-child(10) div').innerText,
+                    );
+                    const team1Blk = window.parseInt(
+                      team1Row.querySelector('td:nth-child(11) div').innerText,
+                    );
+                    const team1To = window.parseInt(
+                      team1Row.querySelector('td:nth-child(12) div').innerText,
+                    );
+                    const team2Row = matchupWallHeader.querySelector(
+                      'tbody tr:nth-child(2)',
+                    );
+                    const team2Id = team2Row
+                      .querySelector('td:nth-child(1) span > a')
+                      .getAttribute('href')
+                      .split('/')[3];
+                    const team2Fgp = window.parseFloat(
+                      team2Row.querySelector('td:nth-child(3) div').innerText,
+                    );
+                    const team2Ftp = window.parseFloat(
+                      team2Row.querySelector('td:nth-child(5) div').innerText,
+                    );
+                    const team2Tptm = window.parseInt(
+                      team2Row.querySelector('td:nth-child(6) div').innerText,
+                    );
+                    const team2Pts = window.parseInt(
+                      team2Row.querySelector('td:nth-child(7) div').innerText,
+                    );
+                    const team2Reb = window.parseInt(
+                      team2Row.querySelector('td:nth-child(8) div').innerText,
+                    );
+                    const team2Ast = window.parseInt(
+                      team2Row.querySelector('td:nth-child(9) div').innerText,
+                    );
+                    const team2Stl = window.parseInt(
+                      team2Row.querySelector('td:nth-child(10) div').innerText,
+                    );
+                    const team2Blk = window.parseInt(
+                      team2Row.querySelector('td:nth-child(11) div').innerText,
+                    );
+                    const team2To = window.parseInt(
+                      team2Row.querySelector('td:nth-child(12) div').innerText,
+                    );
+                    const teamStatById = {
+                      [team1Id]: {
+                        fgp: team1Fgp,
+                        ftp: team1Ftp,
+                        tptm: team1Tptm,
+                        pts: team1Pts,
+                        reb: team1Reb,
+                        ast: team1Ast,
+                        stl: team1Stl,
+                        blk: team1Blk,
+                        to: team1To,
+                      },
+                      [team2Id]: {
+                        fgp: team2Fgp,
+                        ftp: team2Ftp,
+                        tptm: team2Tptm,
+                        pts: team2Pts,
+                        reb: team2Reb,
+                        ast: team2Ast,
+                        stl: team2Stl,
+                        blk: team2Blk,
+                        to: team2To,
+                      },
+                    };
+                    return teamStatById;
+                  }),
+                ),
+              ),
             ])
-              .then(([teams]) => ({
+              .then(([teams, stats]) => ({
                 teamById: teams.reduce(
-                  (prev, curr) => ({ ...prev, [curr.id]: curr }),
+                  (prev, curr) => ({ ...prev, ...curr }),
+                  {},
+                ),
+                teamStatById: stats.reduce(
+                  (prev, curr) => ({ ...prev, ...curr }),
                   {},
                 ),
               }))
-              .then(console.log);
+              .then(data => {
+                port.postMessage({ type: 'DATA', payload: data });
+              });
           })
           .catch(console.log);
         break;
--- src/content.js
+++ src/content.js
@@ -4,36 +4,141 @@ const leagueId = window.location.pathname.split('/')[2];
 
 port.postMessage({ type: 'INIT', payload: { leagueId } });
 
-// port.onMessage.addEventListener(msg => {
-//   console.log(msg);
+port.onMessage.addListener(msg => {
+  const { type, payload } = msg;
+  switch (type) {
+    case 'DATA': {
+      const teamIds = Object.keys(payload.teamById);
 
-//   const matchupHeader = document.querySelector('#matchup-header');
+      const matchupHeader = document.querySelector('#matchup-header');
 
-//   const meDiv = matchupHeader.querySelector('.Grid-u-1-3:nth-child(1)');
-//   const meHref = meDiv.querySelector('a.F-link').getAttribute('href');
+      const meDiv = matchupHeader.querySelector('.Grid-u-1-3:nth-child(1)');
+      const meHref = meDiv.querySelector('a.F-link').getAttribute('href');
+      const meId = meHref.split('/')[3];
 
-//   const opponentDiv = matchupHeader.querySelector('.Grid-u-1-3:nth-child(3)');
-//   const opponentHref = opponentDiv
-//     .querySelector('a.F-link')
-//     .getAttribute('href');
+      const opponentIds = teamIds.filter(id => id !== meId);
 
-//   const numTeams = 10;
+      const opponentDiv = matchupHeader.querySelector(
+        '.Grid-u-1-3:nth-child(3)',
+      );
+      const opponentAvatarImg = opponentDiv.querySelector(
+        'img[class*="Avatar"]',
+      );
+      const opponentNameA = opponentDiv.querySelector('a.F-link');
+      const opponentHref = opponentNameA.getAttribute('href');
+      const opponentUserSpan = opponentDiv.querySelector('span.user-id');
+      const opponentRecordDiv = opponentDiv.querySelector(
+        '.Grid-u:nth-child(2) > :last-child',
+      );
 
-//   const prevTeamArrowBtn = document.createElement('button');
-//   prevTeamArrowBtn.innerHTML = '<';
-//   const nextTeamArrowBtn = document.createElement('button');
-//   nextTeamArrowBtn.innerHTML = '>';
-//   opponentDiv.prepend(prevTeamArrowBtn);
-//   opponentDiv.append(nextTeamArrowBtn);
+      const matchupWallHeader = document.querySelector('#matchup-wall-header');
 
-//   let currOpponentNum = window.parseInt(opponentHref.split('/')[3]);
-//   prevTeamArrowBtn.addEventListener('click', () => {
-//     currOpponentNum -= 1;
-//     currOpponentNum = Math.max(currOpponentNum, 1);
-//     const prevTeamPath = `${opponentHref
-//       .split('/')
-//       .slice(0, 3)
-//       .join('/')}/${currOpponentNum}`;
-//     fetch(prevTeamPath).then(console.log);
-//   });
-// });
+      const meRow = matchupWallHeader.querySelector('tbody tr:nth-child(1)');
+      const meFgpTd = meRow.querySelector('td:nth-child(3)');
+      const meFtpTd = meRow.querySelector('td:nth-child(5)');
+      const meTptmTd = meRow.querySelector('td:nth-child(6)');
+      const mePtsTd = meRow.querySelector('td:nth-child(7)');
+      const meRebTd = meRow.querySelector('td:nth-child(8)');
+      const meAstTd = meRow.querySelector('td:nth-child(9)');
+      const meStlTd = meRow.querySelector('td:nth-child(10)');
+      const meBlkTd = meRow.querySelector('td:nth-child(11)');
+      const meToTd = meRow.querySelector('td:nth-child(12)');
+
+      const opponentRow = matchupWallHeader.querySelector(
+        'tbody tr:nth-child(2)',
+      );
+      const opponentRowNameA = opponentRow.querySelector(
+        'td:nth-child(1) span > a',
+      );
+      const opponentRowAvatarImg = opponentRow.querySelector(
+        'td:nth-child(1) img',
+      );
+      const opponentFgpTd = opponentRow.querySelector('td:nth-child(3)');
+      const opponentFtpTd = opponentRow.querySelector('td:nth-child(5)');
+      const opponentTptmTd = opponentRow.querySelector('td:nth-child(6)');
+      const opponentPtsTd = opponentRow.querySelector('td:nth-child(7)');
+      const opponentRebTd = opponentRow.querySelector('td:nth-child(8)');
+      const opponentAstTd = opponentRow.querySelector('td:nth-child(9)');
+      const opponentStlTd = opponentRow.querySelector('td:nth-child(10)');
+      const opponentBlkTd = opponentRow.querySelector('td:nth-child(11)');
+      const opponentToTd = opponentRow.querySelector('td:nth-child(12)');
+      const opponentFgpDiv = opponentRow.querySelector('td:nth-child(3) div');
+      const opponentFtpDiv = opponentRow.querySelector('td:nth-child(5) div');
+      const opponentTptmDiv = opponentRow.querySelector('td:nth-child(6) div');
+      const opponentPtsDiv = opponentRow.querySelector('td:nth-child(7) div');
+      const opponentRebDiv = opponentRow.querySelector('td:nth-child(8) div');
+      const opponentAstDiv = opponentRow.querySelector('td:nth-child(9) div');
+      const opponentStlDiv = opponentRow.querySelector('td:nth-child(10) div');
+      const opponentBlkDiv = opponentRow.querySelector('td:nth-child(11) div');
+      const opponentToDiv = opponentRow.querySelector('td:nth-child(12) div');
+
+      meFgpTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      meFtpTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      meTptmTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      mePtsTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      meRebTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      meAstTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      meStlTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      meBlkTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      meToTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      opponentFgpTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      opponentFtpTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      opponentTptmTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      opponentPtsTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      opponentRebTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      opponentAstTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      opponentStlTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      opponentBlkTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+      opponentToTd.classList.remove('Fw-b', 'Bg-shade2', 'Bg-selected');
+
+      let currOpponentIdx = opponentIds.findIndex(
+        id => id === opponentHref.split('/')[3],
+      );
+
+      const prevTeamArrowBtn = document.createElement('button');
+      prevTeamArrowBtn.innerHTML = '<';
+      const nextTeamArrowBtn = document.createElement('button');
+      nextTeamArrowBtn.innerHTML = '>';
+      opponentDiv.prepend(prevTeamArrowBtn);
+      opponentDiv.append(nextTeamArrowBtn);
+
+      prevTeamArrowBtn.addEventListener('click', () => {
+        currOpponentIdx -= 1;
+        if (currOpponentIdx === -1) {
+          currOpponentIdx = opponentIds.length - 1;
+        }
+        const currOpponentId = opponentIds[currOpponentIdx];
+        const currOpponent = payload.teamById[currOpponentId];
+        opponentAvatarImg.setAttribute('src', currOpponent.avatarUrl);
+        opponentNameA.innerHTML = currOpponent.name;
+        opponentNameA.setAttribute(
+          'href',
+          `/nba/${leagueId}/${currOpponentId}`,
+        );
+        opponentUserSpan.innerHTML = currOpponent.user;
+        opponentRecordDiv.innerHTML = `${currOpponent.record} | ${currOpponent.standing}`;
+        opponentRowAvatarImg.setAttribute('src', currOpponent.avatarUrl);
+        opponentRowNameA.innerHTML = currOpponent.name;
+        opponentRowNameA.setAttribute(
+          'href',
+          `/nba/${leagueId}/${currOpponentId}`,
+        );
+        const currOpponentStat = payload.teamStatById[currOpponentId];
+        opponentFgpDiv.innerHTML = currOpponentStat.fgp.toString().slice(1);
+        opponentFtpDiv.innerHTML = currOpponentStat.ftp.toString().slice(1);
+        opponentTptmDiv.innerHTML = currOpponentStat.tptm;
+        opponentPtsDiv.innerHTML = currOpponentStat.pts;
+        opponentRebDiv.innerHTML = currOpponentStat.reb;
+        opponentAstDiv.innerHTML = currOpponentStat.ast;
+        opponentStlDiv.innerHTML = currOpponentStat.stl;
+        opponentBlkDiv.innerHTML = currOpponentStat.blk;
+        opponentToDiv.innerHTML = currOpponentStat.to;
+      });
+
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+});
